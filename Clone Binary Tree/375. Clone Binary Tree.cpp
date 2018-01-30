971acf3dc657af8945ebe86ae5c4929f43b6bf83
zhuwh9 <15347556575@163.com>
2018-01-30 19:05:37 +0800

    375

---
+++ Clone Binary Tree/375. Clone Binary Tree.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+class TreeNode {
+public:
+	int val;
+	TreeNode *left, *right;
+	TreeNode(int val) {
+		this->val = val;
+		this->left = this->right = NULL;
+	}
+};
+TreeNode * cloneTree(TreeNode * root) {
+	if (root == NULL) {
+		return NULL;
+	}
+	TreeNode * node = new TreeNode(root->val);
+	node->left = cloneTree(root->left);
+	node->right = cloneTree(root->right);
+	return node;
+}
