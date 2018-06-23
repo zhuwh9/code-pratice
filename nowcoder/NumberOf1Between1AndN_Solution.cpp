2f6f31cbb3434cda73e25b48ca6e58e49ad1fbf4
Zhu Wenhao <15347556575@163.com>
2018-06-23 22:04:13 +0800

    Create NumberOf1Between1AndN_Solution.cpp

---
+++ nowcoder/NumberOf1Between1AndN_Solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int NumberOf1(int n) {
+        int count = 0;
+        while (n != 0) {
+            if (n % 10 == 1) {
+                count++;
+            }
+            n /= 10;
+        }
+        return count;
+    }
+    int NumberOf1Between1AndN_Solution(int n)
+    {
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            count += NumberOf1(i);
+        }
+        return count;
+    }
+};
